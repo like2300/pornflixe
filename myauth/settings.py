012f918fcc4a1494b6e10203b57f92972488faa6
Omer fils Elenga <omerlinks@MacBook-Air-de-Omer.local>
2025-06-03 13:59:43 +0100

    host mail google

--- myauth/settings.py
+++ myauth/settings.py
@@ -8,7 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
+ALLOWED_HOSTS =  [
+    '*'
+]
 
 
 
