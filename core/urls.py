e9274e403832ccf6362427f5ca3db275a7cf19fb
Omer fils Elenga <omerlinks@MacBook-Air-de-Omer.local>
2025-06-03 13:01:06 +0100

    style

--- core/urls.py
+++ core/urls.py
@@ -1,20 +1,21 @@
 from django.urls import include, path
 from .views import *
 from django.views.generic import RedirectView
-
-
+from django.conf import settings
+from django.conf.urls.static import static
+# media
 
 # ================= static urls 
-
 
 static_urls = [
     path('', RedirectView.as_view(url='/home', permanent=False)),
     path('home', HomeView.as_view(), name='home'),
 ]
  
-
-
-
 urlpatterns = [
     path('', include(static_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
