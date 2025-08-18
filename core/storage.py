c0e49e48a084b448c4c602095a428a5b6f5b37de
like2300 <elengaomerfils@gmail.com>
2025-08-18 03:28:46 +0100

    fix: mew commit update for claudflare4

--- core/storage.py
+++ core/storage.py
@@ -1,26 +1,23 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     """
-    Stockage pour les fichiers statiques.
-    Les fichiers sont mis en cache pour une longue durée.
+    Stockage pour les fichiers statiques sur R2.
+    Définit le dossier de destination et les en-têtes de cache.
+    Toutes les autres configurations (bucket, clés, domaine) sont héritées de settings.py.
     """
     location = 'static'
     default_acl = 'public-read'
     object_parameters = {
-        'CacheControl': 'max-age=31536000', # 1 an
+        'CacheControl': 'max-age=31536000',  # Cache pour 1 an
     }
-    # Utilise le domaine CDN pour générer les URLs
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
 
 class MediaStorage(S3Boto3Storage):
     """
-    Stockage pour les fichiers médias.
-    Les fichiers ne sont pas écrasés et sont publics.
+    Stockage pour les fichiers médias sur R2.
+    Définit le dossier de destination.
+    Toutes les autres configurations (bucket, clés, domaine) sont héritées de settings.py.
     """
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
-    # Utilise le domaine CDN pour générer les URLs
-    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
